069f5c9edf90e940750839db39eeeb33ccc9669a
Erfan Gh <parsyab1@gmail.com>
2025-02-06 16:18:53 +0330

    Cleanup, domserver port configurability (#2)
    * REformat the code, Add more comments
    
    * Make the echo outputs more readable
    
    * Make the domserver port configurable
    
    * make the password file in the local directory
    
    * Add eletro DNS
    
    ---------
    
    Co-authored-by: Erfan Ghofrani <erfan.ghofrani.ch@gmail.com>

--- build/startup/scripts/start.sh
+++ build/startup/scripts/start.sh
@@ -1,33 +1,42 @@
 #!/bin/bash
-# setup domjudge ,generate password
+# Setup DOMjudge and generate passwords
+
 set -e
 
-FilePath="/passwords.txt"
+# Set Electro DNS servers in /etc/resolv.conf
+echo "Setting DNS servers..."
+echo "nameserver 10.202.10.10" > /etc/resolv.conf
+echo "nameserver 10.202.10.11" >> /etc/resolv.conf
+
+# File to store generated passwords
+FilePath="./passwords.txt"
 touch $FilePath
 echo "" > $FilePath
-writeFile() 
-{
- echo "" >> $FilePath
- echo $@ >> $FilePath
+
+# Function to write to the password file
+writeFile() {
+  echo "" >> $FilePath
+  echo $@ >> $FilePath
 }
 
-generate_randomPassword() 
-{
+# Function to generate a random password
+generate_randomPassword() {
   local password=$(tr -dc 'A-Za-z0-9' < /dev/urandom | head -c 10)
   echo "$password"
 }
 
+# Generate MySQL root and user passwords
 mysqlRootPassword=$(generate_randomPassword)
 mysqlPassword=$(generate_randomPassword)
 
+# Get the number of judge hosts from the user
+read -p "Please input the number of judge hosts: " judgehost_number
 
-# Get the number of judgehost 
-read -p "Pls input the number of judgeHosts!: " judgehost_number
+# Get the port for DOMjudge server from the user
+read -p "Please input the port for DOMjudge server: " domserver_port
 
-### mariadb
-#docker run -it --restart unless-stopped --name dj-mariadb -e MYSQL_ROOT_PASSWORD=$mysqlRootPassword -e MYSQL_USER=domjudge -e MYSQL_PASSWORD=$mysqlPassword -e MYSQL_DATABASE=domjudge -p 13306:3306 mariadb --max-connections=1000 --innodb-log-file-size=2G --max-allowed-packet=1G
-
-### MariaDB setup
+# Start MariaDB container
+echo "Starting MariaDB container..."
 docker run -dit --restart unless-stopped --name dj-mariadb \
   -e MYSQL_ROOT_PASSWORD="$mysqlRootPassword" \
   -e MYSQL_USER=domjudge \
@@ -38,19 +47,21 @@ docker run -dit --restart unless-stopped --name dj-mariadb \
   --innodb-log-file-size=2G \
   --max-allowed-packet=1G
 
-echo $mysqlRootPassword 
-echo $mysqlPassword
-writeFile "mysqlRootPassword: $mysqlRootPassword "
-writeFile "mysqlPassword: $mysqlPassword "
+# Output MySQL passwords and write them to the file
+echo "MySQL root password: $mysqlRootPassword"
+echo "MySQL user password: $mysqlPassword"
+writeFile "mysqlRootPassword: $mysqlRootPassword"
+writeFile "mysqlPassword: $mysqlPassword"
 
-
+# Wait for MariaDB to be ready
 echo "Waiting for MariaDB to be ready..."
 until docker exec dj-mariadb mariadb -u root -p"$mysqlRootPassword"; do
   sleep 2
 done
 echo "MariaDB is ready!"
 
-### domserver
+# Start DOMjudge server container
+echo "Starting DOMjudge server container..."
 docker run -dit \
   --restart unless-stopped \
   --link dj-mariadb:mariadb \
@@ -60,32 +71,39 @@ docker run -dit \
   -e MYSQL_DATABASE=domjudge \
   -e MYSQL_PASSWORD=$mysqlPassword \
   -e MYSQL_ROOT_PASSWORD=$mysqlRootPassword \
-  -p 80:80 \
+  -p $domserver_port:80 \
   --name domserver \
   domjudge/domserver:latest
 
-echo "Waiting for Domjudge server to be ready..."
-until curl -s 127.0.0.1:80 > /dev/null; do
+# Wait for DOMjudge server to be ready
+echo "Waiting for DOMjudge server to be ready..."
+until curl -s 127.0.0.1:$domserver_port > /dev/null; do
   sleep 2
 done
-echo "Domjudge server is ready!"
+echo "DOMjudge server is ready!"
 
+# Retrieve admin and judgehost passwords from the DOMjudge server
+echo "Retrieving admin and judgehost passwords..."
 admin_password=$(docker exec -it domserver cat /opt/domjudge/domserver/etc/initial_admin_password.secret)
 judgehost_password=$(docker exec -it domserver cat /opt/domjudge/domserver/etc/restapi.secret | awk '/judgehost/ {print $4}')
 
+# Write admin and judgehost passwords to the file
 writeFile "admin_password ${admin_password}"
 writeFile "judgehost_password ${judgehost_password}"
 
-
+# Start judge host containers
+echo "Starting judge host containers..."
 for (( c=0; c<$judgehost_number; c++ ))
 do
-# JudgeHost 
-docker run -dit --privileged \
-  -v /sys/fs/cgroup:/sys/fs/cgroup \
-  --name judgehost-$c \
-  --link domserver:domserver \
-  --hostname judgedaemon-$c \
-  -e DAEMON_ID=$c \
-  -e JUDGEDAEMON_PASSWORD="$judgehost_password" \
-  domjudge/judgehost:latest
+  docker run -dit --privileged \
+    -v /sys/fs/cgroup:/sys/fs/cgroup \
+    --name judgehost-$c \
+    --link domserver:domserver \
+    --hostname judgedaemon-$c \
+    -e DAEMON_ID=$c \
+    -e JUDGEDAEMON_PASSWORD="$judgehost_password" \
+    domjudge/judgehost:latest
+  echo "Judge host $c started."
 done
+
+echo "Setup complete. All containers are up and running."
