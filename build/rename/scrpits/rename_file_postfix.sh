68514c2a73b790058c583bda81624265ef04e3be
mamamamad <mamadhub8@gmail.com>
2025-04-01 19:58:28 +0330

    update

--- build/rename/scrpits/rename_file_postfix.sh
+++ build/rename/scrpits/rename_file_postfix.sh
@@ -1,44 +1,67 @@
-#1/bin/bash
-
-if [ -e $1 ]; then
-cd $1
-for i in $(ls) ; do
-
+#!/bin/bash
+set -e
+if [ -e "$1" ]; then
+cd "$1"
+for i in * ; do
+if [ -d "$i" ]; then
+echo "$i"
 cd "$i"
-cd Data
+# check the data folder
+if [ -d "Data" ]; then
+mv Data data
+echo "the Data folder convert to data."
+fi
+
+if [ -d "data" ]; then
+echo "the data folder is exist"
+cd data
+else
+echo "the data folder is not exist in $i"
+continue
+fi 
+
+if [ -d "secret" ]; then
+# check the secret folder
+echo "the secret folder is exist"
+elif [ -d "sample" ]; then
+# check the sample folder
+echo "the sample folder is exist"
+else
 mkdir secret
 mkdir sample
-# for in folder
-cd in 
-for j in $(ls) ; do 
-mv $j $j.in
+fi
+if [ -d "in" ]; then
+cd in
+for j in * ; do
+mv "$j" "$j.in"
 done
-mv *sample*.in ../sample
-mv * ../secret
-rmdir ../in
-
-# for out folder
+mv *sample*.in ../sample 2>/dev/null || true
+mv * ../secret 2>/dev/null || true
+cd ..
+rmdir in
+if [ -d "../../Data/out" ]; then
 cd ../../Data/out
-for j in $(ls) ; do 
-mv $j $j.ans
+for j in * ; do
+mv "$j" "$j.ans"
+done
+mv *sample*.ans ../sample 2>/dev/null || true
+mv * ../secret 2>/dev/null || true
+cd ..
+rmdir out
+fi
 done
 mv *sample*.ans ../sample
 mv * ../secret
 rmdir ../out
 
-
-
 cd ../..
-
 cd ..
-
+echo "the $i is ok"
 
 done 
+# the end of loop
 echo complete
-
 else
-
- echo "cant find the file"
- exit 0
-
+echo "cant find the file"
+exit 0
 fi
