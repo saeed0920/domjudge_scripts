db1978fee244538cd1179d5e7d60ea663c4e0434
mamamamad <mamadhub8@gmail.com>
2025-04-01 20:34:49 +0330

    update and fix bugsss

--- build/rename/scrpits/rename_file_postfix.sh
+++ build/rename/scrpits/rename_file_postfix.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -e
-if [ -e "$1" ]; then
-cd "$1"
-for i in * ; do
-if [ -d "$i" ]; then
-echo "$i"
+if [ -e $1 ]; then
+cd $1
+for i in $(ls) ; do
+echo $i
 cd "$i"
 # check the data folder
 if [ -d "Data" ]; then
@@ -23,32 +22,27 @@ fi
 if [ -d "secret" ]; then
 # check the secret folder
 echo "the secret folder is exist"
-elif [ -d "sample" ]; then
+fi
+if [ -d "sample" ]; then
 # check the sample folder
 echo "the sample folder is exist"
 else
 mkdir secret
 mkdir sample
 fi
-if [ -d "in" ]; then
-cd in
-for j in * ; do
-mv "$j" "$j.in"
+# for in folder
+cd in 
+for j in $(ls) ; do 
+mv $j $j.in
 done
-mv *sample*.in ../sample 2>/dev/null || true
-mv * ../secret 2>/dev/null || true
-cd ..
-rmdir in
-if [ -d "../../Data/out" ]; then
-cd ../../Data/out
-for j in * ; do
-mv "$j" "$j.ans"
-done
-mv *sample*.ans ../sample 2>/dev/null || true
-mv * ../secret 2>/dev/null || true
-cd ..
-rmdir out
-fi
+mv *sample*.in ../sample
+mv * ../secret
+rmdir ../in
+
+# for out folder
+cd ../out
+for j in $(ls) ; do 
+mv $j $j.ans
 done
 mv *sample*.ans ../sample
 mv * ../secret
