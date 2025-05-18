329fb16113ff8f17c7395b823459dfc34157396e
Saeed Abadiyan <sd.eed1381@gmail.com>
2025-05-18 12:05:02 +0330

    replace the snap with apt

--- scripts/main/setup.sh
+++ scripts/main/setup.sh
@@ -31,20 +31,21 @@ echo "Check if snap installed"
 snap install hello-world
 checkValid $?
 
-# setup docker
+cat "78.157.42.101" > /etc/resolv.conf
 # Add Docker's official GPG key:
-#install -m 0755 -d /etc/apt/keyrings -y
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-#chmod a+r /etc/apt/keyrings/docker.asc
-# Add the repository to Apt sources:
-#echo \
-#  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-#  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-#   tee /etc/apt/sources.list.d/docker.list > /dev/null
-#apt-get update && apt update
-#apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-snap install docker
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 groupadd docker
 usermod -aG docker $username
