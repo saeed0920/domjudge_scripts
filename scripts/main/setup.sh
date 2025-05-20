743756745c000667fda9920b5bf7a88e1168f980
mamamamad <mamadhub8@gmail.com>
2025-05-20 12:33:03 +0330

    replace the jammy mirror for install docker

--- scripts/main/setup.sh
+++ scripts/main/setup.sh
@@ -34,19 +34,26 @@ checkValid $?
 echo "nameserver 178.22.122.100
 nameserver 185.51.200.2" > /etc/resolv.conf
 # Add Docker's official GPG key:
+
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt install ca-certificates curl gnupg lsb-release
 sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
+
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
+  https://download.docker.com/linux/ubuntu jammy stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+
+# Install Docker Engine, CLI, and Containerd:
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo docker run hello-world
+
 
 groupadd docker
 usermod -aG docker $username
