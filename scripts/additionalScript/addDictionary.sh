4b8383c74403b9e858f12d3a0a74bfaf27ff139c
mamamamad <mamadhub8@gmail.com>
2025-05-21 12:33:28 +0330

    add the comments.

--- scripts/additionalScript/addDictionary.sh
+++ scripts/additionalScript/addDictionary.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/bash
 echo "please run with root user"
+sudp apt update 
 sudo apt install stardict
 sudo apt install unrar
+
+echo "Download the farsi dictionary."
+
 wget https://pdn.sharezilla.ir/d/software/StarDict.English.To.Farsi.hfarsi_p30download.com.rar
 
 unrar e -r StarDict.English.To.Farsi.hfarsi_p30download.com.rar -pwww.p30download.com
@@ -9,3 +13,5 @@ unrar e -r StarDict.English.To.Farsi.hfarsi_p30download.com.rar -pwww.p30downloa
 sudo cp Hfarsi_p30download.com.ifo Hfarsi_p30download.com.idx.oft Hfarsi_p30download.com.idx Hfarsi_p30download.com.dict /usr/share/stardict/dic/stardict-dict/
 
 sudo usermod -aG sudo stardict
+
+echo "OK."
